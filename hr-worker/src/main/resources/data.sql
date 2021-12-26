9cf1ef887c6bf08f5b67be4623ccdaea6aa44afc
flipChandler <felipe.santos11@hotmail.com>
2021-12-26 19:52:07 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_worker (name, daily_income) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Alex', 250.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Peter', 400.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Mary Jane', 500.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Gwen', 20.0);
